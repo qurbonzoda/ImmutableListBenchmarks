4d744308de9252556dc095120d34a01748631c12
Abdukodiri Kurbonzoda <bigdreamer@yandex-team.ru>
2018-02-24 20:33:26 +0300

    run treePVector benchmarks

--- build_script.sh
+++ build_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IMPL="rrbTree"
+IMPL="treePVector"
 COMMIT_MESSAGE="$IMPL-add-remove-benchmarks"
 RESULT_FILE_PATH="results/tc-ec2-34-242-6-107/$(date +%Y.%m.%d-%H:%M:%S)-$COMMIT_MESSAGE.csv"
 java -jar target/benchmarks.jar $IMPL.Add $IMPL.Remove $IMPL.AddRemove -wi 10 -i 20 -r 1 -w 1 -prof gc -rf csv -rff $RESULT_FILE_PATH
