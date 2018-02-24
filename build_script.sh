ace3f917126779ca8f896f5d7c99224c38cb6c2c
Abdukodiri Kurbonzoda <bigdreamer@yandex-team.ru>
2018-02-24 12:24:30 +0300

    run baseline benchmarks

--- build_script.sh
+++ build_script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-COMMIT_MESSAGE="commit-message"
-IMPL="persistentDeque"
-RESULT_FILE_PATH="results/$(date +%Y.%m.%d-%H:%M:%S)-$IMPL-add-remove-$COMMIT_MESSAGE.csv"
-java -jar target/benchmarks.jar $IMPL.Add $IMPL.Remove -wi 10 -i 10 -prof gc -rf csv -rff $RESULT_FILE_PATH
+IMPL="baseline"
+COMMIT_MESSAGE="$IMPL-add-remove-benchmarks"
+RESULT_FILE_PATH="results/tc-ec2-34-242-6-107/$(date +%Y.%m.%d-%H:%M:%S)-$COMMIT_MESSAGE.csv"
+java -jar target/benchmarks.jar $IMPL.Add $IMPL.Remove $IMPL.AddRemove -wi 10 -i 20 -r 1 -w 1 -prof gc -rf csv -rff $RESULT_FILE_PATH
 git add results/
 git -c user.name='qurbonzoda' -c user.email='a.qurbonzoda@gmail.com' commit -m $COMMIT_MESSAGE
