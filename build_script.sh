02b966d9f8928361fa0614994559729f19dcaf33
Abdukodiri Kurbonzoda <bigdreamer@yandex-team.ru>
2018-02-25 03:11:51 +0300

    run persistentDeque `fixed size buffers` benchmarks

--- build_script.sh
+++ build_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 IMPL="persistentDeque"
-COMMIT_MESSAGE="$IMPL-add-remove-a-small-cleanup-benchmarks"
+COMMIT_MESSAGE="$IMPL-add-remove-fixed-size-buffers-benchmarks"
 RESULT_FILE_PATH="results/tc-ec2-34-242-6-107/$(date +%Y.%m.%d-%H:%M:%S)-$COMMIT_MESSAGE.csv"
 java -jar target/benchmarks.jar $IMPL.Add $IMPL.Remove $IMPL.AddRemove -wi 10 -i 20 -r 1 -w 1 -prof gc -rf csv -rff $RESULT_FILE_PATH
 git add results/
