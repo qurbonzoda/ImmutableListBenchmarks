d7be060aa9de38f25c70dc367dbdb3716075fb3d
Abdukodiri Kurbonzoda <bigdreamer@yandex-team.ru>
2018-02-25 12:57:35 +0300

    run `stack based buffers size 25` benchmarks

--- build_script.sh
+++ build_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 IMPL="persistentDeque"
-COMMIT_MESSAGE="$IMPL-add-remove-stack-based-buffers-initial-benchmarks"
+COMMIT_MESSAGE="$IMPL-add-remove-stack-based-buffers-size-25-benchmarks"
 RESULT_FILE_PATH="results/tc-ec2-34-242-6-107/$(date +%Y.%m.%d-%H:%M:%S)-$COMMIT_MESSAGE.csv"
 java -jar target/benchmarks.jar $IMPL.Add $IMPL.Remove $IMPL.AddRemove -wi 10 -i 20 -r 1 -w 1 -prof gc -rf csv -rff $RESULT_FILE_PATH
 git add results/
