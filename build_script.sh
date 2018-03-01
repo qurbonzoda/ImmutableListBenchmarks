71cb403a5d255d1553adae14ba9e2997221ba9f9
Abdukodiri Kurbonzoda <bigdreamer@yandex-team.ru>
2018-03-01 07:09:20 +0300

    don't commit benchmark results back to repo.

--- build_script.sh
+++ build_script.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 IMPL="persistentDeque"
-COMMIT_MESSAGE="$IMPL-add-remove-initial-size-9-benchmarks"
-RESULT_FILE_PATH="results/small-deque-optimization/$(date +%Y.%m.%d-%H:%M:%S)-$COMMIT_MESSAGE.csv"
+COMMIT_MESSAGE="initial-size-9-benchmarks"
+RESULT_FILE_PATH="results/small-deque-optimization/$(date +%Y.%m.%d-%H:%M:%S)-$IMPL-add-remove-$COMMIT_MESSAGE.csv"
 java -jar target/benchmarks.jar $IMPL.Add $IMPL.Remove $IMPL.AddRemove -wi 10 -i 20 -r 1 -w 1 -prof gc -rf csv -rff $RESULT_FILE_PATH
-git add results/
-git -c user.name='qurbonzoda' -c user.email='a.qurbonzoda@gmail.com' commit -m $COMMIT_MESSAGE
