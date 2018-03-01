5e8e0a4d8b477d2cea9f391b53adfb2f2559ea7f
Abdukodiri Kurbonzoda <bigdreamer@yandex-team.ru>
2018-03-01 07:27:07 +0300

    run `small deque optimization initial size 15`

--- build_script.sh
+++ build_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 IMPL="persistentDeque"
-COMMIT_MESSAGE="initial-size-12-benchmarks"
+COMMIT_MESSAGE="initial-size-15-benchmarks"
 RESULT_FILE_PATH="results/small-deque-optimization/$(date +%Y.%m.%d-%H:%M:%S)-$IMPL-add-remove-$COMMIT_MESSAGE.csv"
 java -Xmx8192m -jar target/benchmarks.jar $IMPL.Add $IMPL.Remove $IMPL.AddRemove -wi 10 -i 20 -r 1 -w 1 -prof gc -rf csv -rff $RESULT_FILE_PATH
