48f8af938b44e3b355ee380df59747379d056e44
Abdukodiri Kurbonzoda <bigdreamer@yandex-team.ru>
2018-03-01 07:10:38 +0300

    run benchmarks with -Xmx8192m

--- build_script.sh
+++ build_script.sh
@@ -3,4 +3,4 @@
 IMPL="persistentDeque"
 COMMIT_MESSAGE="initial-size-9-benchmarks"
 RESULT_FILE_PATH="results/small-deque-optimization/$(date +%Y.%m.%d-%H:%M:%S)-$IMPL-add-remove-$COMMIT_MESSAGE.csv"
-java -jar target/benchmarks.jar $IMPL.Add $IMPL.Remove $IMPL.AddRemove -wi 10 -i 20 -r 1 -w 1 -prof gc -rf csv -rff $RESULT_FILE_PATH
+java -Xmx8192m -jar target/benchmarks.jar $IMPL.Add $IMPL.Remove $IMPL.AddRemove -wi 10 -i 20 -r 1 -w 1 -prof gc -rf csv -rff $RESULT_FILE_PATH
