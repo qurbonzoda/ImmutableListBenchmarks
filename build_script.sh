c4d828abd96d7749df2f8490007b51bde107531c
Abdukodiri Kurbonzoda <bigdreamer@yandex-team.ru>
2018-03-14 22:42:30 +0300

    run `persistentDeque.Add` benchmarks after cleanups

--- build_script.sh
+++ build_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 IMPL="persistentDeque"
-COMMIT_MESSAGE="flatten-lhs-and-rhs-size-25-benchmarks"
+COMMIT_MESSAGE="cleanup-size-25-benchmarks"
 RESULT_FILE_PATH="results/small-deque-optimization/$(date +%Y.%m.%d-%H:%M:%S)-$IMPL-add-remove-$COMMIT_MESSAGE.csv"
-java -Xmx8192m -jar target/benchmarks.jar $IMPL.Add $IMPL.Remove $IMPL.AddRemove -wi 10 -i 20 -r 1 -w 1 -prof gc -rf csv -rff $RESULT_FILE_PATH
+java -Xmx8192m -jar target/benchmarks.jar $IMPL.Add -wi 10 -i 20 -r 1 -w 1 -prof gc -rf csv -rff $RESULT_FILE_PATH
