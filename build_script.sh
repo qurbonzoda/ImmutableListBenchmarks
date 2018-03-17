a4469136e18a3fcc5d627d0cb06edd863bcfc9f3
Abdukodiri Kurbonzoda <bigdreamer@yandex-team.ru>
2018-03-17 23:01:59 +0300

    remove commas from build_script.sh

--- build_script.sh
+++ build_script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -Xmx8192m -jar target/benchmarks.jar Add.* AddRemove.*, Remove.*, Iterate.* -wi 10 -i 10 -r 500ms -w 500ms -prof gc -rf csv -rff "results/add-remove-iterate-benchmarks.csv"
+java -Xmx8192m -jar target/benchmarks.jar Add.* AddRemove.* Remove.* Iterate.* -wi 10 -i 10 -r 500ms -w 500ms -prof gc -rf csv -rff "results/add-remove-iterate-benchmarks.csv"
