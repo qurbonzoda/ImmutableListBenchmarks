c6a4c25039d52746327f7d2da2e38d66e7da9cbd
Abdukodiri Kurbonzoda <bigdreamer@yandex-team.ru>
2018-03-20 11:57:23 +0300

    run stack buffers of size 7, 8, 9, 12, 13 and 19 implementations

--- build_script.sh
+++ build_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-java -Xmx8192m -jar target/benchmarks.jar Add.* AddRemove.* Remove.* Iterate.* \
+java -Xmx8192m -jar target/benchmarks.jar persistentDeque.Add.* persistentDeque.AddRemove.* persistentDeque.Remove.* persistentDeque.Iterate.* \
     -wi 10 -i 10 -r 500ms -w 500ms \
-    -p impl=INITIAL_IMPL,STACK_25_IMPL,ARRAY_25_IMPL,ARRAY_25S_IMPL \
+    -p impl=STACK_7_IMPL,STACK_8_IMPL,STACK_9_IMPL,STACK_12_IMPL,STACK_13_IMPL,STACK_19_IMPL,STACK_19B_IMPL \
     -prof gc \
-     -rf csv -rff "results/add-remove-iterate-25-benchmarks.csv"
+     -rf csv -rff "results/add-remove-iterate-7-8-9-12-13-19-benchmarks.csv"
