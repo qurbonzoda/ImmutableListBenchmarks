14a077ed9afb9b0b3d5b91f7cb629af1e4ccf744
Abdukodiri Kurbonzoda <bigdreamer@yandex-team.ru>
2018-03-20 18:00:00 +0300

    run baseline benchmarks once more

--- build_script.sh
+++ build_script.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-java -Xmx8192m -jar target/benchmarks.jar persistentDeque.Add.* persistentDeque.AddRemove.* persistentDeque.Remove.* persistentDeque.Iterate.* \
+java -Xmx8192m -jar target/benchmarks.jar baseline.Add.* baseline.AddRemove.* baseline.Remove.* baseline.Iterate.* \
     -wi 10 -i 10 -r 500ms -w 500ms \
-    -p impl=STACK_7_IMPL,STACK_8_IMPL,STACK_9_IMPL,STACK_12_IMPL,STACK_13_IMPL,STACK_19_IMPL,STACK_19B_IMPL \
     -prof gc \
-     -rf csv -rff "results/add-remove-iterate-7-8-9-12-13-19-benchmarks.csv"
+     -rf csv -rff "results/add-remove-iterate-baseline-benchmarks.csv"
