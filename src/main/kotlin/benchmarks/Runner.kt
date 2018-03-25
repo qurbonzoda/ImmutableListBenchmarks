4c8ad89b286f9da31b529439b85e309db0507ac4
Abdukodiri Kurbonzoda <bigdreamer@yandex-team.ru>
2018-03-25 20:15:11 +0300

    run all benchmarks except persistentDeque

--- src/main/kotlin/benchmarks/Runner.kt
+++ src/main/kotlin/benchmarks/Runner.kt
@@ -9,33 +9,36 @@ import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 fun main(args: Array<String>) {
-    val impl = "persistentDeque"
-    val benchTitle = ""
-    val outputFile = "teamcityArtifacts/${currentDateTime()}-$impl-$benchTitle.csv"
 
-    val fileWriter = FileWriter(outputFile)
+    val impls = listOf(
+            "arrayList",
+            "baseline",
+            "clojurePVector",
+            "dexxPVector",
+            "javaSlangPVector",
+            "scalaPVector",
+            "rrbTree",
+            "treePVector"/*,
+            "persistentDeque"*/
+    )
 
-    for (arg in args) {
-        fileWriter.appendln(arg)
+    for (impl in impls) {
+        val outputFile = "teamcityArtifacts/${currentDateTime()}-$impl.csv"
+
+        val options = OptionsBuilder()
+                .include("$impl.Add.*")
+                .include("$impl.Remove.*")
+                .include("$impl.AddRemove.*")
+                .include("$impl.Iterate.*")
+                .warmupIterations(10)
+                .measurementIterations(10)
+                .warmupTime(TimeValue.milliseconds(500))
+                .measurementTime(TimeValue.milliseconds(500))
+                .addProfiler("gc")
+
+        val runResults = Runner(options.build()).run()
+        printResults(runResults, impl, outputFile)
     }
-
-    fileWriter.flush()
-    fileWriter.close()
-
-//    val options = OptionsBuilder()
-////            .include("$impl.Add.addLast$")
-//            .include("$impl.Add.*")
-//            .include("$impl.Remove.*")
-//            .include("$impl.AddRemove.*")
-//            .warmupIterations(10)
-//            .measurementIterations(10)
-//            .warmupTime(TimeValue.seconds(1))
-//            .measurementTime(TimeValue.seconds(1))
-////            .param("listSize", BM_1000000)
-//            .addProfiler("gc")
-//
-//    val runResults = Runner(options.build()).run()
-//    printResults(runResults, impl, outputFile)
 }
 
 fun currentDateTime(): String {
@@ -45,15 +48,14 @@ fun currentDateTime(): String {
 }
 
 fun printResults(runResults: Collection<RunResult>, implementation: String, outputFile: String) {
-    val csvHeader = "Implementation,Method,listSize,Score,Score Error,Allocation Rate"
+    val csvHeader = "Implementation,Method,listSize,bufferImpl,Score,Score Error,Allocation Rate"
 
     val fileWriter = FileWriter(outputFile)
 
     fileWriter.appendln(csvHeader)
 
-    for (result in runResults) {
-        val row = csvRowFrom(result, implementation)
-        fileWriter.appendln(row)
+    runResults.forEach {
+        fileWriter.appendln(csvRowFrom(it, implementation))
     }
 
     fileWriter.flush()
@@ -64,9 +66,10 @@ fun csvRowFrom(result: RunResult, implementation: String): String {
     val nanosInMillis = 1000
     val method = result.primaryResult.getLabel()
     val listSize = result.params.getParam("listSize").toInt()
+    val impl = result.params.getParam("impl")
     val score = result.primaryResult.getScore() * nanosInMillis / listSize
     val scoreError = result.primaryResult.getScoreError() * nanosInMillis / listSize
     val allocationRate = result.secondaryResults["·gc.alloc.rate.norm"]!!.getScore() / listSize
 
-    return "$implementation,$method,$listSize,%.3f,%.3f,%.3f".format(score, scoreError, allocationRate)
+    return "$implementation,$method,$listSize,$impl,%.3f,%.3f,%.3f".format(score, scoreError, allocationRate)
 }
